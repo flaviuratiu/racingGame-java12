17218c683a31381baa4ff50cf3279da8ab51d01c
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-06-19 19:06:00 +0300

    studied constructors

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,7 +5,10 @@ package org.fasttrackit;
  */
 public class App {
     public static void main(String[] args) {
-        Car car = new Car();
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+
+        Car car = new Car(engine);
         car.name = "Dacia";
         car.color = "blue";
         car.doorCount = 4;
@@ -13,16 +16,11 @@ public class App {
         car.fuelLevel = 50;
         car.running = true;
 
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-
-        car.engine = engine;
-
         System.out.println("Our engine...");
         System.out.println(car.engine.manufacturer);
 
 
-        Car car2 = new Car();
+        Car car2 = new Car(new Engine());
         car2.name = "Lada";
         car2.color = "black";
         car2.doorCount = 2;
